7c0e4b5ab9074f5e457308d927f6768236c18996
lliangchenc <lliangchenc@163.com>
2017-12-02 22:58:56 +0800

    rewrite the vga interface and finish building charset

--- vga/vga.vhd
+++ vga/vga.vhd
@@ -33,12 +33,17 @@ entity vga is
 	port(
 		clk: in std_logic;
 		rst: in std_logic; 
-		data_in: in std_logic_vector(18 downto 0);
-		control:in std_logic;
+		--data_in: in std_logic_vector(18 downto 0);
+		pos_in: in std_logic_vector(15 downto 0);
+		data_in: in std_logic_vector(15 downto 0);
+		--pos, data使能
+		WE_i_1, WE_i_2: in std_logic;
+		--control:in std_logic;
 		ram_data: inout std_logic_vector(15 downto 0);
 		ram_addr: out std_logic_vector(17 downto 0);
 		EN, OE, WE: out std_logic;
 		HS, VS: out std_logic;
+		WE_o_1, WE_o_2: out std_logic;
 		R : out std_logic_vector (2 downto 0);
 		G : out std_logic_vector (2 downto 0);
 		B : out std_logic_vector (2 downto 0)
@@ -47,44 +52,61 @@ end vga;
 
 -- 640 * 480 @60MHz
 -- divided into 80 * 30 blocks;
+-- 56 * 21 blocks;
 
 architecture Behavioral of vga is
 
-type screen_info is array (80 downto 0, 30 downto 0) of std_logic_vector(6 downto 0);
+--显存
+type screen_info is array (79 downto 0, 29 downto 0) of std_logic_vector(15 downto 0);
 signal screen : screen_info;
 
-constant start_addr: std_logic_vector(18 downto 0) := ???;
+
+constant start_addr: std_logic_vector(17 downto 0) := "000000000000000000";
 constant img_size: integer := 128;
+
+--block计数
 signal H_count : integer := 0;
 signal V_count : integer := 0;
+
+--当前输出的块的x, y及颜色信息
 signal block_x : integer := 0;
 signal block_y : integer := 0;
+signal R_block : integer := 0;
+signal G_block : integer := 0;
+signal B_block : integer := 0;
+
+--当前像素相对于block的坐标
 signal inblock_x : integer := 0;
 signal inblock_y : integer := 0;
 
+--输入x, y坐标
 signal in_x : integer := 0;
 signal in_y : integer := 0;
 signal block_info : std_logic_vector(6 downto 0);
 
 begin
+	
 	R <= ram_data(2 downto 0);
 	G <= ram_data(5 downto 3);
 	B <= ram_data(8 downto 6);
 	
 	EN <= '1';
 	WE <= '1';
-	OE <= not(control);
+	-- OE <= not(control);
+	OE <= '0';
 	
 	block_x <= V_count / 8;
 	block_y <= H_count / 16;
 	inblock_x <= V_count - 8 * block_x;
 	inblock_y <= H_count - 16 * block_y;
+	R_block <= conv_integer(screen(block_x)(block_y)(15 downto 13));
+	G_block <= conv_integer(screen(block_x)(block_y)(12 downto 10));
+	B_block <= conv_integer(screen(block_x)(block_y)(9 downto 7));
 	
-	in_x <= conv_integer(data_in(18 downto 12));
-	in_y <= conv_integer(data_in(11 downto 7));
-	block_info <= data_in(6 downto 0);
+	in_x <= conv_integer(addr_in(15 downto 8));
+	in_y <= conv_integer(addr_in(7 downto 0));
 	
-	ram_addr <= start_addr + conv_integer(screen(block_x)(block_y)) * img_size + inblock_x + inblock_y * 8;
+	ram_addr <= start_addr + conv_integer(screen(block_x)(block_y)(6 downto 0)) * img_size + inblock_x + inblock_y * 8;
 	
 	update : process(H_count)
 		begin
@@ -102,8 +124,13 @@ begin
 				
 			elsif(rising_edge(clk))then
 				V_count <= (V_count + 1) mod 800;
-				if(control = 1)then
-					screen(in_x)(in_y) <= block_info;
+				if(WE_i_1 = '1' and WE_i_2 = '1')then
+					screen(in_x)(in_y) <= data_in;
+					WE_o_1 <= '0';
+					WE_o_2 <= '0';
+				else
+					WE_o_1 <= '1';
+					WE_o_2 <= '1';
 				end if;
 			end if;
 		end process;
