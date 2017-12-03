2c9f5be8a63c0f7cfee30e1775305241fcf4a160
lliangchenc <lliangchenc@163.com>
2017-12-04 01:04:36 +0800

    update the charset

--- charset/png2bit.py
+++ charset/png2bit.py
@@ -13,12 +13,13 @@ print(f_num)
 
 for file in os.listdir(os.getcwd() + "/charset"):
     img = cv2.imread("charset/" + file)
-    img = cv2.resize(img, (16, 8), interpolation=cv2.INTER_CUBIC)
+    img = cv2.resize(img, (8, 16), interpolation=cv2.INTER_CUBIC)
+    cv2.imwrite("temp/" + file , img)
     shape = np.shape(img)
     print(shape)
-    for i in range(shape[1]):
-        for j in range(shape[0]):
-            if(img[j][i][0] > 128):
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            if(img[i][j][0] > 200):
                 bit_file.write(struct.pack("H", 0x0000))
             else:
                 bit_file.write(struct.pack("H", 0xFFFF))
