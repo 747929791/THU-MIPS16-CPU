0e68451402974a88e6699bb548baad908654a178
QinYue <747929791@qq.com>
2017-12-06 21:08:37 +0800

    补充了中断按键测试程序

--- Assembler/KeyBoard.s
+++ Assembler/KeyBoard.s
@@ -1,19 +1,70 @@
-GOTO KeyBoard_Test
+LOAD_ADDR KeyBoard_Test_IntProgram R0
+MTIH R0 ;指定中断处理程序
+GOTO KeyBoard_Test_Int
 
-KeyBoard_Test:
+KeyBoard_Test_NoInt:  ;测试轮训的键盘处理
   LI R5 BF
   SLL R5 R5 0
-  Loop:
+  KeyBoard_Test_NoInt_Loop:
     CALL KeyBoard_Get
-    BEQZ R0 Loop
+    BEQZ R0 KeyBoard_Test_NoInt_Loop
     NOP
     MOVE R1 R0
     MOVE R0 R5
     CALL COM_WRITE
-    B Loop
+    B KeyBoard_Test_NoInt_Loop
     NOP
   RET
   
+KeyBoard_Test_Int:  ;测试带中断的键盘处理
+  LI R5 BF
+  SLL R5 R5 0
+  KeyBoard_Test_Int_Loop:
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    B KeyBoard_Test_Int_Loop
+    NOP
+  LI R1 F0
+  RET
+
+KeyBoard_Test_IntProgram:   ;按键测试中断处理程序
+  SW_SP R6 7E  ;约定SP+127以上为不安全区域，禁止使用
+  ADDSP 7F
+  SAVE_REG
+  CALL KeyBoard_Get
+  MOVE R1 R0
+  LI R0 BF
+  SLL R0 R0 0
+  LOAD_REG
+  ADDSP 81
+  LW_SP R6 7E
+  INT F
+  NOP
+
 ;--------------------------------------------键盘控制程序--------------------------------------------
 KeyBoard_Get:   ;从键盘读取当前内容到R0
   DATA KeyBoard_Last 1
