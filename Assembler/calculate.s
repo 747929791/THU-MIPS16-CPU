ca615e20bb6a4278f6dcf9583729b6a43694891b
747929791 <747929791@qq.com>
2017-12-06 18:57:59 +0800

    实现了汇编计算器calculate

--- Assembler/calculate.s
+++ Assembler/calculate.s
@@ -1,48 +1,54 @@
-;这是一个DOS命令行管理主程序
-GOTO Root_Main
+;这是一个文本计算器(输入"34*100"输出"3400")
+GOTO Calculate_Main
 
+;STRING CALC_TEST_S "123*41" ;测试表达式
+STRING CALC_TEST_S "10/3E" ;测试表达式
+DATA CALC_RESULT 10  ;存放计算结果
+DATA CALC_RESULT_END 1 ;结果缓冲区的结尾
 DATA KeyBoard_Cache 100 ;缓存当前未完成的输入的内容
 DATA KeyBoard_Cache_P 1 ;记录缓存区下一个字符的地址
+STRING Calculate_Input_SIG ">>> "
 
-Root_Main:
+Calculate_Test:
+  Load_addr CALC_TEST_S R0
+  CALL Calculate_Calc
+  RET
+
+Calculate_Main:
   CALL VGA_MEM_INIT
-  CALL Root_INIT
+  CALL Calculate_INIT
   CALL VGA_COM_PRINT
-  Root_Main_Command_Input:
-  LOAD_ADDR sys_root_path R0
+  LOAD_ADDR Calculate_Input_SIG R0   ;先输出">>> "
   CALL printf
-;LI R5 BF
-;SLL R5 R5 0
-;ADDIU R5 6
-;LI R4 32
-;SW R5 R4 0
-  Root_Main_KeyBoard_Get_Loop:
+LOAD_ADDR CALC_TEST_S R2
+  Calculate_Main_KeyBoard_Get_Loop:
     CALL KeyBoard_Get
-    BEQZ R0 Root_Main_KeyBoard_Get_Loop
+;LW R2 R0 0
+;ADDIU R2 1
+    BEQZ R0 Calculate_Main_KeyBoard_Get_Loop
     NOP
     LI R6 0A
     CMP R0 R6   ;判断是否为回车
-    BTEQZ Root_Main_KeyBoard_Get_Enter ;是回车
+;LI R6 0
+;CMP R0 R6
+    BTEQZ Calculate_Main_KeyBoard_Get_Enter ;是回车
     NOP
     CALL print_char  ;否则输出该字符
     LOAD_DATA KeyBoard_Cache_P R1 0
     SW R1 R0 0
     ADDIU R1 1
     SAVE_DATA KeyBoard_Cache_P R1 0
-;LI R3 0A
-;SW R5 R3 0
-    B Root_Main_KeyBoard_Get_Loop
+    B Calculate_Main_KeyBoard_Get_Loop
     NOP
-    Root_Main_KeyBoard_Get_Enter:
-        CALL Root_Main_KeyBoard_Enter
-;ADDIU R4 1
-;SW R5 R4 0
+    Calculate_Main_KeyBoard_Get_Enter:
+        CALL Calculate_Main_KeyBoard_Enter
     CALL VGA_COM_PRINT
-    B Root_Main_KeyBoard_Get_Loop
+;RET
+    B Calculate_Main_KeyBoard_Get_Loop
     NOP
   RET
 
-Root_Main_KeyBoard_Enter:   ;当按下键盘回车时应当处理的逻辑
+Calculate_Main_KeyBoard_Enter:   ;当按下键盘回车时应当处理的逻辑
   SAVE_REG
   ;清空缓存区，补\0
   CALL next_cursor_line
@@ -51,34 +57,144 @@ Root_Main_KeyBoard_Enter:   ;当按下键盘回车时应当处理的逻辑
   SW R0 R1 0
   LOAD_ADDR KeyBoard_Cache R0
   SAVE_DATA KeyBoard_Cache_P R0 0
-  ;如果输入L，进入生命游戏
-  Load_Data KeyBoard_Cache R0 0
-  ADDIU R0 BF   ;R0-=ord(L)
-  BEQZ R0 GOTO_LifeGame
-  NOP
-  ;均不为以上指令
-  Load_Addr KeyBoard_Cache R0
+  ;计算输入表达式结果，并显示
+  CALL Calculate_Calc
   CALL printf
-  Load_Addr sys_command_not_found R0
-  CALL printf
-  ;换行两行，输出"command not found.",并输出一个"A:\>"
   CALL next_cursor_line
-  CALL next_cursor_line
-  Load_Addr sys_root_path R0
+  Load_Addr Calculate_Input_SIG R0
   CALL printf
   LOAD_REG
   RET
 
-GOTO_NotePad:
-  ;CALL NotePad
-  GOTO Root_Main
-GOTO_LifeGame:
-  ;GOTO LifeGame_Main
-  GOTO Root_Main
-GOTO_RetroSnake:
-  ;GOTO RetroSnake_Main
-  GOTO Root_Main
+Calculate_Calc:     ;根据R0指向地址的字符串计算表达式的值，并将结果字符串首地址通过R0返回
+  SAVE_REG
+  ;将操作数放于堆栈上
+  LI R4 0 ;R4记录当前正在计算的操作数，R3记录左操作数
+  LI R5 FF ;R5运算符
+  LW R0 R1 0   ;将字符读入R1
+  Calculate_Calc_Loop:    
+    ;如果当前值不为数字则默认为运算符
+    LI R6 39
+    SLT R6 R1
+    BTNEZ Calculate_Calc_Find_Operator
+    NOP
+    LI R6 30
+    SLT R1 R6
+    BTNEZ Calculate_Calc_Find_Operator
+    NOP
+    ;否则为数字
+    B Calculate_Calc_Find_Num
+    NOP    
+    Calculate_Calc_Find_Operator:
+      LI R6 FF
+      CMP R5 R6
+      BTNEZ 3  ;跳过这一计算阶段
+      NOP
+      MOVE R3 R4
+      LI R4 0
+      MOVE R5 R1
+      B Calculate_Calc_Loop_Final
+      NOP
+    Calculate_Calc_Find_Num:
+      LI R6 30
+      SUBU R1 R6 R1  ;R1 ASCII转整数
+      SLL R6 R4 1
+      SLL R4 R4 3
+      ADDU R4 R1 R4
+      ADDU R4 R6 R4 ;R4=10*R4+ord(R0)-48
+      B Calculate_Calc_Loop_Final
+      NOP
+    Calculate_Calc_Loop_Final:
+      ADDIU R0 1
+      LW R0 R1 0   ;将字符读入R1
+      BNEZ R1 Calculate_Calc_Loop
+      NOP
+  ;现在R3和R4位两个操作数的值，R5为操作符
+  LI R6 2B
+  CMP R5 R6
+  BTEQZ Calculate_Calc_Plus
+  LI R6 2D
+  CMP R5 R6
+  BTEQZ Calculate_Calc_Minus
+  LI R6 2A
+  CMP R5 R6
+  BTEQZ Calculate_Calc_Multi
+  LI R6 2F
+  CMP R5 R6
+  BTEQZ Calculate_Calc_Division
+  LI R6 25
+  CMP R5 R6
+  BTEQZ Calculate_Calc_Mod
+  LI R6 5E
+  CMP R5 R6
+  BTEQZ Calculate_Calc_Power
+  NOP
+;在此进行错误处理
+    STRING Calculate_Calc_Syntax_Error "Syntax Error"
+    LOAD_ADDR Calculate_Calc_Syntax_Error R0
+    SW_SP R0 F8
+    LOAD_REG
+    RET
+    
+  Calculate_Calc_Plus:
+    ADDU R3 R4 R0
+    B Calculate_Calc_GetResult
+    NOP
+  Calculate_Calc_Minus:
+    SUBU R3 R4 R0
+    B Calculate_Calc_GetResult
+    NOP
+  Calculate_Calc_Multi:
+    MOVE R0 R3
+    MOVE R1 R4
+    CALL MULTI
+    B Calculate_Calc_GetResult
+    NOP
+  Calculate_Calc_Division:
+    MOVE R0 R3
+    MOVE R1 R4
+    CALL DIVISION
+    B Calculate_Calc_GetResult
+    NOP
+  Calculate_Calc_Mod:
+    MOVE R0 R3
+    MOVE R1 R4
+    CALL DIVISION
+    MOVE R0 R1
+    B Calculate_Calc_GetResult
+    NOP
+  Calculate_Calc_POWER:
+    MOVE R0 R3
+    MOVE R1 R4
+    CALL POWER
+    B Calculate_Calc_GetResult
+    NOP
+  Calculate_Calc_GetResult:
+  ;此时R0保存计算结果
+  LOAD_ADDR CALC_RESULT_END R5 ;R5缓存写入字符的地址
+  ADDIU R5 FF
+  LI R4 0
+  SW R5 R4 0  ;最后一位为\0
+  Calculate_Calc_GetResult_L1:
+   ADDIU R5 FF
+   LI R1 0A
+   CALL DIVISION
+   ADDIU R1 30
+   SW R5 R1 0
+   BNEZ R0 Calculate_Calc_GetResult_L1
+   NOP
+  SW_SP R5 F8
+  LOAD_REG
+  RET
 
+Calculate_INIT:     ;初始化的屏幕字符显示
+  SAVE_REG
+  LOAD_ADDR KeyBoard_Cache R0
+  SAVE_DATA KeyBoard_Cache_P R0 0
+  LI R0 0
+  CALL set_cursor
+  LOAD_REG
+  RET
 
 set_cursor:     ;设置输入光标坐标为R0(16)位，用于系统文字输出
   DATA CURSOR_X 1
@@ -162,62 +278,6 @@ next_cursor_line:    ;光标下移一行，越界后到达下一行行首，若超出屏幕则滚屏
   LW_SP R0 0
   LW_SP R1 1
   RET
-
-
-STRING sys_s1 "Preparing to start your computer."
-STRING sys_s2 "This may take a few minuts. Please wait..."
-STRING sys_s3 " "
-STRING sys_s4 "The diagnostic tools were successfully loaded to drive G."
-STRING sys_s5 " "
-STRING sys_s6 "MSCDEX Version 2.25."
-STRING sys_s7 "Copyright (C) Tsinghua University. 2017. All rights reserved."
-STRING sys_s8 "       Drive H: = Driver MSCD001 unit 0"
-STRING sys_s9 " "
-STRING sys_s10 "To get help, type HELP and press ENTER."
-STRING sys_root_path "A:\> "
-STRING sys_command_not_found " command not found."
-
-Root_INIT:     ;初始化的屏幕字符显示
-  SAVE_REG
-  LOAD_ADDR KeyBoard_Cache R0
-  SAVE_DATA KeyBoard_Cache_P R0 0
-  LI R0 0
-  SAVE_DATA CURSOR_X R0 0
-  SAVE_DATA CURSOR_Y R0 0
-  LOAD_ADDR sys_s1 r0
-  CALL printf
-  CALL next_cursor_line
-  LOAD_ADDR sys_s2 r0
-  CALL printf
-  CALL next_cursor_line
-  LOAD_ADDR sys_s3 r0
-  CALL printf
-  CALL next_cursor_line
-  LOAD_ADDR sys_s4 r0
-  CALL printf
-  CALL next_cursor_line
-  LOAD_ADDR sys_s5 r0
-  CALL printf
-  CALL next_cursor_line
-  LOAD_ADDR sys_s6 r0
-  CALL printf
-  CALL next_cursor_line
-  LOAD_ADDR sys_s7 r0
-  CALL printf
-  CALL next_cursor_line
-  LOAD_ADDR sys_s8 r0
-  CALL printf
-  CALL next_cursor_line
-  LOAD_ADDR sys_s9 r0
-  CALL printf
-  CALL next_cursor_line
-  LOAD_ADDR sys_s10 r0
-  CALL printf
-  CALL next_cursor_line
-  CALL next_cursor_line
-  LOAD_REG
-  RET
-
 
 ;——————————————————————————————————————————————————————————————VGA模块——————————————————————————————————————————————————————————
 ;VGA显示控制器，内置显存 
@@ -431,6 +491,24 @@ DIVISION:  ;加减交替原码一位除法，R0/R1，商保存于R0，余数存于R1
   LW_SP R2 0
   LW_SP R3 1
   LW_SP R4 2
+  RET
+
+POWER:   ;计算R0^R1,返回R0(16位)
+  SAVE_REG
+  MOVE R3 R1
+  MOVE R2 R0
+  LI R0 1
+  BEQZ R1 POWER_LOOP_RET
+  NOP
+  POWER_LOOP:
+    MOVE R1 R2
+    CALL MULTI
+    ADDIU R3 FF
+    BNEZ R3 POWER_LOOP
+    NOP
+  POWER_LOOP_RET:
+  SW_SP R0 F8
+  LOAD_REG
   RET
 
 FastRAND:  ;快速的伪随机数发生器，将15位结果返回至寄存器R0
