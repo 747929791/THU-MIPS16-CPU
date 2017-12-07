48fad2fca6b722af2fba3efb47bda7427432daba
zfw <zfw15@mails.tsinghua.edu.cn>
2017-12-07 15:23:17 +0800

    sliced oversized procedure

--- Assembler/Chat.s
+++ Assembler/Chat.s
@@ -31,75 +31,7 @@ Chat_Main:
   SAVE_DATA CHAT_COM_BUFFER_P R0 0
 
   Chat_Main_KeyBoard_Get_Loop:
-	Chat_Main_Comm_Get:		;读串口
-		LI R0 BF
-		SLL R0 R0 0
-		CALL COM_READ
-		LI R1 0				;是0则结束
-		CMP R0 R1
-		BTEQZ Chat_Main_Keyboard_Get
-		NOP
-		LI R1 20			;是回车则输出
-		CMP R0 R1
-		BTEQZ Chat_Print_Com
-		NOP
-		LOAD_DATA CHAT_COM_BUFFER_P R1 0		;读入一个字符
-		SW R1 R0 0
-		ADDIU R1 1
-		SAVE_DATA CHAT_COM_BUFFER_P R1 0
-		B Chat_Main_Comm_Get
-		NOP
-		
-	Chat_Print_Com:
-		SAVE_REG
-		;判断是否为空
-		Load_Data CHAT_COM_BUFFER_P R0 0
-		LOAD_ADDR CHAT_COM_BUFFER R1
-		CMP R0 R1
-		BTNEZ Chat_Print_Com_Process
-		NOP
-		B Chat_Main_Keyboard_Get
-		NOP
-
-		;保存坐标
-		LOAD_DATA CURSOR_X R0 0
-		SAVE_DATA CHAT_INPUT_X R0 0
-		LOAD_DATA CURSOR_Y R0 0
-		SAVE_DATA CHAT_INPUT_Y R0 0
-		
-		;补\0
-		Chat_Print_Com_Process:
-		LI R1 0
-		SW R0 R1 0
-
-		;移动坐标
-		LOAD_DATA CHAT_TEXT_X R0 0
-		LI R1 0
-		SAVE_DATA CURSOR_X R0 0
-		SAVE_DATA CURSOR_Y R1 0
-		LOAD_ADDR chat_friend R0
-		CALL printf
-		CALL next_cursor_line
-		LOAD_ADDR CHAT_COM_BUFFER R0
-		CALL printf
-		CALL next_cursor_line
-
-		;清空缓存区
-		LOAD_ADDR CHAT_COM_BUFFER R0
-		SAVE_DATA CHAT_COM_BUFFER_P R0 0
-
-		;保存各处坐标信息
-		Chat_Main_Com_RET:
-			LOAD_DATA CURSOR_X R0 0
-			SAVE_DATA CHAT_TEXT_X R0 0
-			LOAD_DATA CHAT_INPUT_X R0 0
-			LOAD_DATA CHAT_INPUT_Y R1 0
-			SAVE_DATA CURSOR_X R0 0
-			SAVE_DATA CURSOR_Y R1 0
-		LOAD_REG
-		B Chat_Main_Keyboard_Get
-		NOP
-	
+	CALL Chat_Main_Comm_Get
 	Chat_Main_Keyboard_Get:
 		CALL KeyBoard_Get
 		BEQZ R0 Chat_Main_KeyBoard_Get_Loop
@@ -263,3 +195,76 @@ Chat_INIT:     ;初始化的屏幕字符显示
   SAVE_DATA CHAT_TEXT_X R0 0
   LOAD_REG
   RET
+  
+Chat_Main_Comm_Get:		;读串口
+	SAVE_REG
+	LI R0 BF
+	SLL R0 R0 0
+	CALL COM_READ
+	LI R1 0				;是0则结束
+	CMP R0 R1
+	BTEQZ Chat_Main_Comm_Ret
+	NOP
+	LI R1 20			;是回车则输出
+	CMP R0 R1
+	BTNEZ Chat_Main_Read_Char
+	NOP
+	;判断是否为空
+	Load_Data CHAT_COM_BUFFER_P R0 0
+	LOAD_ADDR CHAT_COM_BUFFER R1
+	CMP R0 R1
+	BTEQZ Chat_Main_Comm_Ret
+	NOP
+	CALL Chat_Print_Com
+	B Chat_Main_Comm_Ret
+	
+Chat_Main_Read_Char:
+	LOAD_DATA CHAT_COM_BUFFER_P R1 0		;读入一个字符
+	SW R1 R0 0
+	ADDIU R1 1
+	SAVE_DATA CHAT_COM_BUFFER_P R1 0  
+  
+Chat_Main_Comm_Ret:
+	LOAD_REG
+	RET
+  
+Chat_Print_Com:
+	SAVE_REG
+
+	;保存坐标
+	LOAD_DATA CURSOR_X R0 0
+	SAVE_DATA CHAT_INPUT_X R0 0
+	LOAD_DATA CURSOR_Y R0 0
+	SAVE_DATA CHAT_INPUT_Y R0 0
+	
+	;补\0
+	Chat_Print_Com_Process:
+	LI R1 0
+	SW R0 R1 0
+
+	;移动坐标
+	LOAD_DATA CHAT_TEXT_X R0 0
+	LI R1 0
+	SAVE_DATA CURSOR_X R0 0
+	SAVE_DATA CURSOR_Y R1 0
+	LOAD_ADDR chat_friend R0
+	CALL printf
+	CALL next_cursor_line
+	LOAD_ADDR CHAT_COM_BUFFER R0
+	CALL printf
+	CALL next_cursor_line
+
+	;清空缓存区
+	LOAD_ADDR CHAT_COM_BUFFER R0
+	SAVE_DATA CHAT_COM_BUFFER_P R0 0
+
+	;保存各处坐标信息
+	Chat_Main_Com_RET:
+		LOAD_DATA CURSOR_X R0 0
+		SAVE_DATA CHAT_TEXT_X R0 0
+		LOAD_DATA CHAT_INPUT_X R0 0
+		LOAD_DATA CHAT_INPUT_Y R1 0
+		SAVE_DATA CURSOR_X R0 0
+		SAVE_DATA CURSOR_Y R1 0
+	LOAD_REG
+	RET
